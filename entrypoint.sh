638a00944f9b16f40f66d529420dd0212bec982c
saisona <alexandre.saison.pro@gmail.com>
2022-06-30 11:41:01 +0200

    Update:
    - requirements.txt are taken from source repo of the Github Workflow in
      mt-repo this was then nutshell-2-7-2.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "::group::Install dependencies"
-python -m pip install --extra-index-url https://$1:$2@pypiserver.inarix.com/simple/ -r requirements.txt
+python -m pip install --extra-index-url https://$1:$2@pypiserver.inarix.com/simple/ metaflow==2.6.3 pnx==3.0.0 kubernetes
 echo "::endgroup::"
 
 echo "::group::Authenticate to cluster"
