8efe60747e99a796ca5b2f35e5034646d0703fa3
saisona <alexandre.saison.pro@gmail.com>
2022-07-12 12:57:19 +0200

    Handle better notification

--- bookish.py
+++ bookish.py
@@ -193,7 +193,7 @@ class ModelDeployment(FlowSpec):
             print(f"Waiting for {tts}s")
             time.sleep(tts)
         
-        if status == "Missing" or status == "Degraded":
+        if status != "Healthy":
             raise RuntimeError(f"Health status error: {status} after all retries")
 
     def checkApplicationExists(self) -> bool:
